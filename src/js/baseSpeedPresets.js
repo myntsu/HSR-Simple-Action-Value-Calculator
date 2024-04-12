ea09d5808ad145ace02811b3d125db3847ebae23
Myntsu <dingodille16@hotmail.com>
2024-04-11 22:07:10 -0400

    Added a search inside the form to filter characters

--- src/js/baseSpeedPresets.js
+++ src/js/baseSpeedPresets.js
@@ -1,28 +1,47 @@
-// Fetch the JSON data
+import { handlePresetButtons } from './cyclesBonusPresets.js';
+
 fetch('characters.json')
-  .then(response => response.json())
-  .then(data => {
-    let container = document.querySelector('#characters-preset');
+.then(response => response.json())
+.then(data => {
+  let container = document.querySelector('#characters-preset');
+  
+  data.forEach(character => {
+    let button = document.createElement('button');
+    button.classList.add('presets-buttons');
+    button.setAttribute('data-cycles', character.speed);
+    button.setAttribute('data-input', 'character');
+    button.setAttribute('data-name', character.character.toLowerCase());
+    
+    let img = document.createElement('img');
+    img.classList.add('character-image');
+    let imageName = character.image.replace(/[()]/g, '').replace(/\./g, '').replace(/&/g, 'and').replace(/[^a-zA-Z0-9]/g, '_');
+    img.src = `./characters/${imageName}.webp`;
+    img.alt = character.character;
+    if (character.stars === 'five') {
+      img.classList.add('five-stars');
+    } else if (character.stars === 'four') {
+      img.classList.add('four-stars');
+    }
+    
+    button.appendChild(img);
+    container.appendChild(button);
+  });
 
-    data.forEach(character => {
-      let button = document.createElement('button');
-      button.classList.add('presets-buttons');
-      button.setAttribute('data-cycles', character.speed);
-      button.setAttribute('data-input', 'character');
+  handlePresetButtons();
+})
+.catch(error => console.error('Error:', error));
 
-      let img = document.createElement('img');
-      img.classList.add('character-image');
-      let imageName = character.image.replace(/[()]/g, '').replace(/\./g, '').replace(/&/g, 'and').replace(/[^a-zA-Z0-9]/g, '_');
-      img.src = `./characters/${imageName}.webp`;
-      img.alt = character.character;
-      if (character.stars === 'five') {
-        img.classList.add('five-stars');
-      } else if (character.stars === 'four') {
-        img.classList.add('four-stars');
-      }
+let searchField = document.querySelector('#searchCharacter');
+searchField.addEventListener('input', function() {
+  let searchTerm = this.value.toLowerCase();
+  let buttons = document.querySelectorAll('.presets-buttons');
 
-      button.appendChild(img);
-      container.appendChild(button);
-    });
-  })
-  .catch(error => console.error('Error:', error));
+  buttons.forEach(button => {
+    let characterName = button.getAttribute('data-name');
+    if (characterName.includes(searchTerm)) {
+      button.style.display = 'block';
+    } else {
+      button.style.display = 'none';
+    }
+  });
+});
--- src/js/cyclesBonusPresets.js
+++ src/js/cyclesBonusPresets.js
@@ -1,30 +1,32 @@
-let presetButtons = document.querySelectorAll('.presets-buttons');
+export function handlePresetButtons() {
+  let presetButtons = document.querySelectorAll('.presets-buttons');
 
-presetButtons.forEach(presetButton => {
-  presetButton.addEventListener('click', function(event) {
-    event.preventDefault();
+  presetButtons.forEach(presetButton => {
+    presetButton.addEventListener('click', function(event) {
+      event.preventDefault();
 
-    if (this.classList.contains('pressed')) {
-      this.classList.remove('pressed');
-      updateInputValue(this, 0);
-      return;
-    }
-
-    presetButtons.forEach(otherButton => {
-      if (otherButton !== this) {
-        otherButton.classList.remove('pressed');
+      if (this.classList.contains('pressed')) {
+        this.classList.remove('pressed');
+        updateInputValue(this, 0);
+        return;
       }
+
+      presetButtons.forEach(otherButton => {
+        if (otherButton !== this) {
+          otherButton.classList.remove('pressed');
+        }
+      });
+
+      this.classList.add('pressed');
+
+      let cycles = this.getAttribute('data-cycles');
+
+      updateInputValue(this, cycles);
     });
-
-    this.classList.add('pressed');
-
-    let cycles = this.getAttribute('data-cycles');
-
-    updateInputValue(this, cycles);
   });
-});
+}
 
-export function updateInputValue(button, value) {
+function updateInputValue(button, value) {
   let inputId = button.getAttribute('data-input');
   let input = document.querySelector(`input#${inputId}`);
   if (input) {
