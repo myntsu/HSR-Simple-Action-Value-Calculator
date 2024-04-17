8a2734e4ad08ba7429c8cd8dfaba596407f093f4
Myntsu <dingodille16@hotmail.com>
2024-04-16 21:22:43 -0400

    Preset % are now additive, and they substract each other when unpressed

--- src/js/cyclesBonusPresets.js
+++ src/js/cyclesBonusPresets.js
@@ -2,7 +2,6 @@ export function handlePresetButtons() {
   let presetButtons = document.querySelectorAll('.presets-buttons');
 
   presetButtons.forEach(presetButton => {
-    // Set aria-pressed to false initially
     presetButton.setAttribute('aria-pressed', 'false');
 
     presetButton.addEventListener('click', function(event) {
@@ -12,38 +11,51 @@ export function handlePresetButtons() {
       let input = document.querySelector(`input#${inputId}`);
       let sameGroupButtons = document.querySelectorAll(`.presets-buttons[data-input='${inputId}']`);
 
-      if (this.classList.contains('pressed')) {
-        this.classList.remove('pressed');
-        updateInputValue(this, 0);
-        this.setAttribute('aria-pressed', 'false');
-        return;
-      }
+      if (inputId === 'percentSpeed') {
+        let cycles = parseInt(this.getAttribute('data-cycles'));
+        let currentValue = parseInt(input.value) || 0;
 
-      sameGroupButtons.forEach(otherButton => {
-        if (otherButton !== this) {
-          otherButton.classList.remove('pressed');
-          otherButton.setAttribute('aria-pressed', 'false');
+        if (this.classList.contains('pressed')) {
+          this.classList.remove('pressed');
+          this.setAttribute('aria-pressed', 'false');
+          updateInputValue(input, currentValue - cycles);
+        } else {
+          this.classList.add('pressed');
+          this.setAttribute('aria-pressed', 'true');
+          updateInputValue(input, currentValue + cycles);
         }
-      });
+      } else {
+        if (this.classList.contains('pressed')) {
+          this.classList.remove('pressed');
+          updateInputValue(input, 0);
+          this.setAttribute('aria-pressed', 'false');
+          return;
+        }
 
-      this.classList.add('pressed');
+        sameGroupButtons.forEach(otherButton => {
+          if (otherButton !== this) {
+            otherButton.classList.remove('pressed');
+            otherButton.setAttribute('aria-pressed', 'false');
+          }
+        });
 
-      let cycles = this.getAttribute('data-cycles');
+        this.classList.add('pressed');
 
-      updateInputValue(this, cycles);
+        let cycles = this.getAttribute('data-cycles');
+
+        updateInputValue(input, cycles);
+
+        this.setAttribute('aria-pressed', 'true');
+      }
 
       if (input) {
         input.focus();
       }
-
-      this.setAttribute('aria-pressed', 'true');
     });
   });
 }
 
-function updateInputValue(button, value) {
-  let inputId = button.getAttribute('data-input');
-  let input = document.querySelector(`input#${inputId}`);
+function updateInputValue(input, value) {
   if (input) {
     input.value = value;
   }
